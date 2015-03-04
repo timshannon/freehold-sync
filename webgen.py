61504fe39ab3ae328112aa7dcb4e3407360cdb6e
Tim Shannon <shannon.timothy@gmail.com>
2015-03-04 15:36:01 -0600

    Going to use go-bindata instead of some js optimizer

--- webgen.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-import subprocess
-import os
-import shutil
-
-#Generates runs r.js optimizer to package an entire webpage into one html file
-# then embeds that html file in a .go file as a string const for building
